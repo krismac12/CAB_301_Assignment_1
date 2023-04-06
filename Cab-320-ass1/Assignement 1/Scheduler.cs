e1461d29d3861add846090b6b870a46d1e244ef0
krismac12 <91189116+krismac12@users.noreply.github.com>
2023-04-06 11:56:07 +1000

    added merge sort

--- Cab-320-ass1/Assignement 1/Scheduler.cs
+++ Cab-320-ass1/Assignement 1/Scheduler.cs
@@ -6,7 +6,7 @@ public class Scheduler : IScheduler {
 	}
 
 	public IJobCollection Jobs { get; }
-    public int iterations = 0;
+    public int operations = 0;
 
 	public IJob[] FirstComeFirstServed() 
     {
@@ -19,10 +19,12 @@ public class Scheduler : IScheduler {
             while (j >= 0 && jobsA[j].TimeReceived > job.TimeReceived)
             {
                 jobsA[j + 1] = jobsA[j];
+                operations++;
                 j = j - 1;
-                iterations++;
             }
             jobsA[j + 1] = job;
+            operations++;
+
         }
         return jobsA;
 
@@ -38,7 +40,74 @@ public class Scheduler : IScheduler {
 
     public IJob[] ShortestJobFirst() {
 
-        //To be implemented by students
-        return Jobs.ToArray();
+        return MergeSort(Jobs).ToArray();
     }
+
+    private IJobCollection MergeSort(IJobCollection collection)
+    {
+        IJob[] jobs = collection.ToArray();
+        int n = (int)collection.Count;
+        if(n <= 1)
+        {
+            return collection;
+        }
+
+        IJobCollection left_jobs = new JobCollection(collection.Count);
+        IJobCollection right_jobs = new JobCollection(collection.Count);
+
+
+        for (int i = 0; i < n / 2; i++)
+        {
+            left_jobs.Add(jobs[i]);
+        }
+        for(int i = n/2;i < n; i++)
+        {
+            right_jobs.Add(jobs[i]);
+        }
+
+        left_jobs = MergeSort(left_jobs);
+        right_jobs = MergeSort(right_jobs);
+
+        return Merge(left_jobs, right_jobs, collection);
+
+    }
+
+    private IJobCollection Merge(IJobCollection left_jobs, IJobCollection right_jobs, IJobCollection original)
+    {
+        JobCollection c = new JobCollection(original.Count);
+
+        IJob[] left = left_jobs.ToArray();
+        IJob[] right = right_jobs.ToArray();
+        while (left_jobs.Count > 0 && right_jobs.Count > 0)
+        {
+            if(left[0].ExecutionTime > right[0].ExecutionTime)
+            {
+                c.Add(right[0]);
+                right_jobs.Remove(right[0].Id);
+            }
+            else
+            {
+                c.Add(left[0]);
+                left_jobs.Remove(left[0].Id);
+            }
+            left = left_jobs.ToArray();
+            right = right_jobs.ToArray();
+        }
+
+        while (left_jobs.Count > 0)
+        {
+            c.Add(left[0]);
+            left_jobs.Remove(left[0].Id);
+            left = left_jobs.ToArray();
+        }
+
+        while (right_jobs.Count > 0)
+        {
+            c.Add(right[0]);
+            right_jobs.Remove(right[0].Id);
+            right = right_jobs.ToArray();
+        }
+        return c;
+    }
+
 }
--- Cab-320-ass1/Program.cs
+++ Cab-320-ass1/Program.cs
@@ -7,31 +7,37 @@ namespace Cab_320_ass1
     {
         static void Main(string[] args)
         {
-            int time1 = TestIterations(10);
-            int time2 = TestIterations(100);
-            int time3 = TestIterations(200);
-            int time4 = TestIterations(300);
-            int time5 = TestIterations(400);
-            int time6 = TestIterations(500);
-            int time7 = TestIterations(600);
-            int time8 = TestIterations(700);
-            int time9 = TestIterations(999);
-            System.Console.WriteLine(time1);
-            System.Console.WriteLine(time2);
-            System.Console.WriteLine(time3);
-            System.Console.WriteLine(time4);
-            System.Console.WriteLine(time5);
-            System.Console.WriteLine(time6);
-            System.Console.WriteLine(time7);
-            System.Console.WriteLine(time8);
-            System.Console.WriteLine(time9);
+            System.Console.WriteLine("\n Unsorted \n");
+
+            JobCollection ran = generateRandom(100);
+
+            IJob[] jobs = ran.ToArray();
+
+            foreach(IJob job in jobs)
+            {
+                Console.WriteLine(job);
+            }
+
+            Scheduler scheduler = new Scheduler(ran);
+
+            IJob[] job_sorted = scheduler.ShortestJobFirst();
+
+            System.Console.WriteLine("\n Sorted \n");
+
+            foreach (IJob job in job_sorted)
+            {
+                Console.WriteLine(job);
+            }
+
+
+
 
 
         }
 
         public static JobCollection generateRandom(int count)
         {
-            JobCollection collection = new JobCollection((uint)count);
+            JobCollection collection = new JobCollection((uint)999);
             Random random = new Random();
             for (int i = 1; i <= count; i++)
             {
@@ -60,9 +66,9 @@ namespace Cab_320_ass1
             JobCollection jobs = generateRandom(n);
 
             Scheduler scheduler = new Scheduler(jobs);
-            IJob[] sortedJobs = scheduler.FirstComeFirstServed();
+            IJob[] sortedJobs = scheduler.ShortestJobFirst();
 
-            return scheduler.iterations;
+            return scheduler.operations;
 
         }
     }
